c5248bcee04ede28d792bb4d25578472fe7cb12e
easontm <easontm@gmail.com>
2016-06-08 04:39:20 +0000

    Cleaning up some stuff

--- scripts/note_refresh.py
+++ scripts/note_refresh.py
@@ -2,15 +2,20 @@ import urllib.request
 import fileinput
 from os import system
 
-# Download the file from `url` and save it locally under `file_name`:
-with urllib.request.urlopen('https://www.dropbox.com/s/nh8o4gou047td5m/Notes.txt?dl=1') as response, open('/home/pi/Cinemagraphs/Notes.txt', 'wb') as out_file:
+localNotePath = "/home/pi/Cinemagraphs/notes/Notes.txt"
+remoteNotePath = "https://www.dropbox.com/s/nh8o4gou047td5m/Notes.txt?dl=1"
+
+# Download the notes from remoteNotePath and save it locally under localNotePath:
+with urllib.request.urlopen(remoteNotePath) as response, open(localNotePath, 'wb') as out_file:
     data = response.read() # a `bytes` object
     out_file.write(data)
 
 
-with fileinput.FileInput("/home/pi/Cinemagraphs/Notes.txt", inplace=True, backup='.bak') as file:
+# fixing line end chars
+with fileinput.FileInput(localNotePath, inplace=True, backup='.bak') as file:
 	for line in file:
 		print(line, end='')
 fileinput.close()
 
+# backs up and updates html files
 system("python3 update_html.py")
